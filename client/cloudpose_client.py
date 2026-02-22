0471e8ecbdc3f8bc6ec728e2326bd22f2a9528a2
Oatsinthepine <zzy20001201@gmail.com>
2026-02-22 11:26:15 +0800

    升级cloudpose_client.py使其支持argparse

--- client/cloudpose_client.py
+++ client/cloudpose_client.py
@@ -9,12 +9,50 @@ import uuid
 import base64
 import json
 import os
+import argparse
+
+
+def parse_argmuments() -> argparse.Namespace:
+    """
+    
+      
+    """
+    parser = argparse.ArgumentParser(
+        description="CloudPose Client - Send parallel requests to pose estimation service"
+    )
+    parser.add_argument(
+        "--input-folder",
+        type=str,
+        required=True,
+        help="Path to input folder containing images"
+    )
+
+    parser.add_argument(
+        "--url",
+        type=str,
+        required=True,
+        help="CloudPose API endpoint URL"
+    )
+
+    parser.add_argument(
+        "--workers",
+        type=int,
+        required=True,
+        help="Number of parallel worker threads"
+    )
+
+    return parser.parse_args()
+
 
 #send http request
-def call_cloudpose_service(image):
+def call_cloudpose_service(image:str, url: str) -> None:
+
+    """
+
+    """
+    
     try:
-        url = str(sys.argv[2])
-        data = {}
+        data = dict()
         #generate uuid for image
         img_id = uuid.uuid5(uuid.NAMESPACE_OID, image)
         # Encode image into base64 string
@@ -39,30 +77,43 @@ def call_cloudpose_service(image):
     except Exception as e:
         print("Exception in webservice call: {}".format(e))
 
+
 # gets list of all images path from the input folder
-def get_images_to_be_processed(input_folder):
+def get_images_to_be_processed(input_folder: str) -> list[str]:
     images = []
     for image_file in glob.iglob(input_folder + "*.jpg"):
         images.append(image_file)
     return images
 
-def main():
-    ## provide argumetns-> input folder, url, number of wrokers
-    if len(sys.argv) != 4:
-        raise ValueError("Arguments list is wrong. Please use the following format: {} {} {} {}".
-                         format("python Cloudpose_client.py", "<input_folder>", "<URL>", "<number_of_workers>"))
 
-    input_folder = os.path.join(sys.argv[1], "")
+
+def main() -> None:
+    """
+    Main function to parse arguments and call the cloudpose service
+    """
+    
+    args = parse_argmuments()
+
+    input_folder = os.path.join(args.input_folder, "")
     images = get_images_to_be_processed(input_folder)
-    num_images = images.__len__()
-    num_workers = int(sys.argv[3])
+
+    url = args.url
+    
+    num_images = len(images)
+    num_workers = args.workers
     start_time = time.time()
+    
     #craete a worker  thread  to  invoke the requests in parallel
     with PoolExecutor(max_workers=num_workers) as executor:
-        for _ in executor.map(call_cloudpose_service,  images):
+        # for _ in executor.map(call_cloudpose_service,  images):
+        for _ in executor.map(lambda img: call_cloudpose_service(img, url), images):
             pass
+
     elapsed_time =  time.time() - start_time
     print("Total time spent: {} average response time: {}".format(elapsed_time, elapsed_time/num_images))
+
+
+
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -10,9 +10,19 @@ All the required packages are part of the basic python installation. Please use 
 
 python cloudpose_client.py  <input folder name> <URL> <num_threads>
 
+## 目前开发的时候用这个命令测试 （我下面打算重构 cloudpose_client.py， 使用argparse，这样更工程化）：
+`python3 ./client/cloudpose_client.py ./client/inputfolder http://localhost:60001/api/pose_estimation 1`
+
+
 ## Sample run command
 
 python cloudpose_client.py  inputfolder/  http://localhost:8000/api/pose_detection 4
+
+## 目前开发的时候仍然用这个命令启动fastapi server：
+`uvicorn cloudpose.app.main:app --host 0.0.0.0 --port 60001`
+
+
+---
 
 ## 为什么做 Service Layer Separation
 
